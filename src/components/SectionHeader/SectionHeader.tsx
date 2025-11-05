6901a6e58ef2a4f86404a31d12be5ed41cf5bb31
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-11-05 02:03:45 +0200

    feat: add SectionHeader component with styles and RightIcon asset

---
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -0,0 +1,28 @@
+import clsx from "clsx"
+import styles from "./SectionHeader.module.css"
+import RightIcon from "./assets/RightIcon.svg"
+
+type SectionHeaderProps = {
+  title: string
+  subtitle?: string
+  href?: string
+}
+
+export function SectionHeader ({ title, subtitle, href }: SectionHeaderProps) {
+  return (
+    <header className={styles.header}>
+      <h2 className={styles.title}>{title}</h2>
+
+      {Boolean(subtitle && href) && (
+        <a className={clsx(styles.subtitle, styles.link)} href={href}>
+          {subtitle}
+          <RightIcon />
+        </a>
+      )}
+
+      {Boolean(subtitle && !href) && (
+        <span className={styles.subtitle}>{subtitle}</span>
+      )}
+    </header>
+  )
+}

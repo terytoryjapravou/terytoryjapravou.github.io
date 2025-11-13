896a0b77779a6350e58f46da144b6ff91fc25992
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-11-14 01:50:32 +0200

    feat: Add DetailsSection component with styling for mission and goals

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,12 @@
 import { PageLayout } from "@site/src/components/PageLayout/PageLayout"
 import { AboutSection } from "./internal/AboutSection/AboutSection"
+import { DetailsSection } from "./internal/DetailsSection/DetailsSection"
 
 export default function AboutPage() {
   return (
     <PageLayout title="Пра нас">
       <AboutSection />
+      <DetailsSection />
     </PageLayout>
   )
 }

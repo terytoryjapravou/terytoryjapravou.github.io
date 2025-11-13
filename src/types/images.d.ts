360c2e685843cdd6c867cdf51f4addf9ed0e0c0e
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-11-13 12:05:51 +0200

    feat: add Docusaurus configuration files and update package scripts for improved plugin management

---
+++ src/types/images.d.ts
@@ -0,0 +1,51 @@
+// Type declarations for image assets
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.avif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.ico' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.bmp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.tiff' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.tif' {
+  const src: string;
+  export default src;
+}

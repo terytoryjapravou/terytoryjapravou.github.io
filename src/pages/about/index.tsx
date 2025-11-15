21fe2e6b54708351a514927ae2fc1c7e0e4097b0
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-11-15 22:34:02 +0200

    feat: Add HistorySection component with responsive design and styling

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,12 +1,14 @@
 import { PageLayout } from "@site/src/components/PageLayout/PageLayout"
 import { AboutSection } from "./internal/AboutSection/AboutSection"
 import { DetailsSection } from "./internal/DetailsSection/DetailsSection"
+import { HistorySection } from "./internal/HistorySection/HistorySection"
 
 export default function AboutPage() {
   return (
     <PageLayout title="Пра нас">
       <AboutSection />
       <DetailsSection />
+      <HistorySection />
     </PageLayout>
   )
 }

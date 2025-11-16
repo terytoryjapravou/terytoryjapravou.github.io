6811395e6463522d464edde04f0f5f377acb4cb0
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-11-16 23:38:51 +0200

    feat: Add ActivityPage and HeaderSection with responsive layout and images

---
+++ src/pages/activity/index.tsx
@@ -0,0 +1,10 @@
+import { PageLayout } from "@site/src/components/PageLayout/PageLayout";
+import { HeaderSection } from "./internal/HeaderSection/HeaderSection";
+
+export default function ActivityPage() {
+  return (
+    <PageLayout title="Дзейнасць">
+      <HeaderSection />
+    </PageLayout>
+  )
+}

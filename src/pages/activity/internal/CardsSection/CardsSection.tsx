d6b9670223f59f6b0fc93a284b1c39cbc6a32fcd
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-11-18 01:22:07 +0200

    feat: add SocialCard component with responsive design and social media links

--- src/pages/activity/internal/CardsSection/CardsSection.tsx
+++ src/pages/activity/internal/CardsSection/CardsSection.tsx
@@ -1,11 +1,13 @@
 import styles from './CardsSection.module.css'
 import { EducationCourseCard } from './internal/EducationCourseCard/EducationCourseCard'
+import { SocialCard } from './internal/SocialCard/SocialCard'
 
 export function CardsSection() {
   return (
     <section className={styles.section}>
       <div className={styles.wrapper}>
         <EducationCourseCard />
+        <SocialCard />
       </div>
     </section>
   )

1969b7018dd4fe705581729f29fbc15a93c8f78f
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-11-19 23:30:35 +0200

    feat: add DonateSection component with styles for donation layout

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -1,10 +1,12 @@
 import { PageLayout } from "@site/src/components/PageLayout/PageLayout"
 import { WhySection } from "./internal/WhySection/WhySection"
+import { DonateSection } from "./internal/DonateSection/DonateSection"
 
 export default function DonatePage() {
   return (
     <PageLayout title="Падтрымаць">
       <WhySection />
+      <DonateSection />
     </PageLayout>
   )
 }
---
+++ src/pages/donate/internal/DonateSection/DonateSection.tsx
@@ -0,0 +1,17 @@
+import { Coffee } from './internal/Coffee/Coffee'
+
+import style from './DonateSection.module.css'
+import { Stripe } from './internal/Stripe/Stripe'
+import { Tax } from './internal/Tax/Tax'
+
+export function DonateSection() {
+  return (
+    < section className={style.section} >
+      <div className={style.wrapper}>
+        <Coffee className={style.coffee} />
+        <Stripe className={style.stripe} />
+        <Tax className={style.tax} />
+      </div>
+    </section >
+  )
+}

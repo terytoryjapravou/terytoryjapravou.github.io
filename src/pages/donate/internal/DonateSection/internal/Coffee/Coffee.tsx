f2a47575455fef7b5b815f4a56a6654390663929
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-11-19 23:30:35 +0200

    Add Stripe donation section with styles and assets
    - Created Stripe.module.css for styling the Stripe donation card.
    - Implemented Stripe.tsx component for rendering the donation card with title, subtitle, image, and tag.
    - Added assets including Image.png, Tick.svg, and underline.png for visual enhancements.
    - Introduced underline image in the title styling for better aesthetics.

---
+++ src/pages/donate/internal/DonateSection/internal/Coffee/Coffee.tsx
@@ -0,0 +1,31 @@
+import clsx from 'clsx'
+
+import styles from './Coffee.module.css'
+import { Tag } from '@site/src/components/Tag/Tag'
+
+type CoffeeProps = {
+  className?: string
+}
+
+export function Coffee({ className }: CoffeeProps) {
+  return (
+    <a href="#" target='_blank' rel="noopener noreferrer" className={clsx(styles.card, className)}>
+      <h3 className={styles.title}>Buy Me a Coffee</h3>
+
+      <span className={styles.subtitle}>Адным клікам можна:</span>
+
+      <ul className={styles.content}>
+        <li>
+          зрабіць разавы данат або падпісацца на штомесячную падтрымка;
+        </li>
+        <li>
+          купіць мерч у нашым шопе – ён не толькі прыгожы і ўтульны,
+          але і дапамагае распаўсюджваць ідэі правоў чалавека.
+          Навінкі з’яўляюцца рэгулярна — сачыце!
+        </li>
+      </ul>
+
+      <Tag className={styles.tag}>Адкрыць Buy Me a coffee</Tag>
+    </a>
+  )
+}

77a60e9338797b903b58610171b4d9513b5dc07a
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-11-19 23:30:35 +0200

    feat: add DonatePage with WhySection component and associated styles and assets

---
+++ src/pages/donate/index.tsx
@@ -0,0 +1,10 @@
+import { PageLayout } from "@site/src/components/PageLayout/PageLayout"
+import { WhySection } from "./internal/WhySection/WhySection"
+
+export default function DonatePage() {
+  return (
+    <PageLayout title="Падтрымаць">
+      <WhySection />
+    </PageLayout>
+  )
+}

49d999882eda19606a51ab65f4d2520126dc6323
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-11-23 22:40:14 +0200

    fix(SectionHeader): change title type from string to ReactNode for better flexibility

--- src/components/SectionHeader/SectionHeader.tsx
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -1,14 +1,15 @@
 import clsx from "clsx"
 import styles from "./SectionHeader.module.css"
 import RightIcon from "./assets/RightIcon.svg"
+import { ReactNode } from "react"
 
 type SectionHeaderProps = {
-  title: string
+  title: ReactNode
   subtitle?: string
   href?: string
 }
 
-export function SectionHeader ({ title, subtitle, href }: SectionHeaderProps) {
+export function SectionHeader({ title, subtitle, href }: SectionHeaderProps) {
   return (
     <header className={styles.header}>
       <h2 className={styles.title}>{title}</h2>

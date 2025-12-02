86e5024dd46754dae57210bf61df48d0e0a5b085
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-12-03 01:31:23 +0200

    feat(ActivityCard): enhance content styling with line height and max-width; update EducationCard to support fullWidth prop

--- src/pages/internal/ActivitySection/internal/ActivityDesktopSection/internal/ActivityCard/ActivityCard.tsx
+++ src/pages/internal/ActivitySection/internal/ActivityDesktopSection/internal/ActivityCard/ActivityCard.tsx
@@ -1,21 +1,26 @@
+import clsx from 'clsx'
 import styles from './ActivityCard.module.css'
 
 type ActivityCardProps = {
   title: string
   content?: React.ReactNode
   preview?: React.ReactNode
+  fullWidth?: boolean
 }
 
 export const ActivityCard = ({
   title,
   content,
   preview,
+  fullWidth = false,
 }: ActivityCardProps) => {
   return (
     <div className={styles.card}>
       <div className={styles.contentWrapper}>
         <h3 className={styles.title}>{title}</h3>
-        <div className={styles.content}>{content}</div>
+        <div className={clsx(styles.content, {
+          [styles.contentFullWidth]: fullWidth,
+        })}>{content}</div>
       </div>
       {preview &&
         <div className={styles.preview}>

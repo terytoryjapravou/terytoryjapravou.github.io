57f887f31f4743492b82f970660c968b48d71f42
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-12-03 01:31:23 +0200

    Refactor code structure for improved readability and maintainability

---
+++ src/pages/internal/ActivitySection/internal/ActivityDesktopSection/internal/WebinarsCard/internal/WebinarsCardPreview/WebinarsCardPreview.tsx
@@ -0,0 +1,24 @@
+import Lamp from './assets/lamp.svg'
+import Illustration from './assets/illystration_1.png'
+import Image from '@theme/IdealImage';
+
+import styles from './WebinarsCardPreview.module.css'
+
+export const WebinarsCardPreview = () => {
+  return (
+    <div className={styles.preview}>
+      <Lamp className={styles.lamp} />
+      <div className={styles.caption}>
+        Мы абмяркоўваем тэмы, што датычаць усіх:
+        ад барацьбы з дыскрымінацыяй да выклікаў штучнага інтэлекту
+        і яго ўплыву на правы чалавека.
+      </div>
+
+
+      <Image
+        img={Illustration}
+        className={styles.image}
+      />
+    </div>
+  )
+}

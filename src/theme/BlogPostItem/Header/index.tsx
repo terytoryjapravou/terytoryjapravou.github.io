16986125ce1764d72d1e49686d8a9f6e3cd16e66
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-12-05 19:36:16 +0200

    fix: refactor BlogPostItemHeader for improved structure and add backLink functionality

--- src/theme/BlogPostItem/Header/index.tsx
+++ src/theme/BlogPostItem/Header/index.tsx
@@ -5,11 +5,19 @@ import BlogPostItemHeaderInfo from '@theme/BlogPostItem/Header/Info'
 import styles from './Header.module.css'
 import { BlogPostHeaderImage } from './BlogPostHeaderImage/BlogPostHeaderImage'
 import { BlogPostItemHeaderTags } from './BlogPostItemHeaderTags/BlogPostItemHeaderTags'
+import { useBlogPost } from '@docusaurus/plugin-content-blog/lib/client/index.js'
 
 export default function BlogPostItemHeader(): JSX.Element {
+  const { metadata: { permalink } } = useBlogPost()
+  const blogId = permalink.split('/')[1]
+
+
   return (
     <header className={styles.header}>
       <div className={styles.headerContent}>
+
+        {blogId === 'opportunities' && <a className={styles.backLink} href="/opportunities">← Усе магчымасці</a>}
+        {blogId === 'articles' && <a className={styles.backLink} href="/articles">← Усе артыкулы</a>}
 
         <div className={styles.headerContentWrapper}>
           <BlogPostItemHeaderTitle />

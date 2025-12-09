1eddf1b282f695282730d6e402b6f2dded064653
Mysterious Seal <i.am.mysterious.seal@gmail.com>
2025-12-09 21:00:40 +0200

    feat: add placeholder images for ArticleCard and OpportunityCard when no image is provided

--- src/components/BlogPostCard/OpportunityCard/OpportunityCard.tsx
+++ src/components/BlogPostCard/OpportunityCard/OpportunityCard.tsx
@@ -3,15 +3,17 @@ import styles from "./OpportunityCard.module.css"
 import { Button } from "@site/src/components/Button/Button"
 import { BlogPostCardProps } from "../BlogPostCardProps"
 import Image from '@theme/IdealImage';
+import Placeholder from './assets/placeholder.jpeg'
 
 export function OpportunityCard({ post }: BlogPostCardProps) {
   return (
     <a href={post.permalink} className={styles.card}>
-      {post.image &&
-        <div className={styles.previewImage}>
-          <Image img={post.image} alt={post.title} />
-        </div>
-      }
+      <div className={styles.previewImage}>
+        {post.image ?
+          <Image img={post.image} alt={post.title} /> :
+          <Image img={Placeholder} />
+        }
+      </div>
 
       {Boolean(post.tags.length) &&
         <div className={styles.tagsGroup}>
